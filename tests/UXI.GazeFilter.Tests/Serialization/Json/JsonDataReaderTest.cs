be3ae585213452972d5c37bea1fdaa848f4b2daa
Martin Konopka <martkono@outlook.com>
2018-03-09 13:28:51 +0100

    added test projects

---
+++ tests/UXI.GazeFilter.Tests/Serialization/Json/JsonDataReaderTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UXI.GazeFilter.Tests.Serialization.Json
+{
+    [TestClass]
+    public class JsonDataReaderTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
