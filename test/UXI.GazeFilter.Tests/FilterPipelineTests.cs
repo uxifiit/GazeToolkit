c74aa9d8f223784cf0c6ab0c25c7b1703701ada4
Martin Konopka <martkono@outlook.com>
2018-11-20 19:09:01 +0100

    added first few tests for FilterPipeline

---
+++ test/UXI.GazeFilter.Tests/FilterPipelineTests.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UXI.GazeFilter;
+using UXI.GazeFilter.Exceptions;
+using UXI.GazeFilter.Fakes;
+
+namespace UXI.GazeFilter
+{
+   
+
+    [TestClass]
+    public class FilterPipelineTests
+    {
+
+        [TestMethod]
+        [ExpectedException(typeof(FilterTypeMismatchException))]
+        public void Initialize_FirstFilterInputTypeDoesNotMatchPipelineInput_ExceptionThrown()
+        {
+            var pipeline = new FilterPipeline<A, B, object>
+            (
+                new NullFilter<B, A, object>(),
+                new NullFilter<A, B, object>()
+            );
+
+            pipeline.Initialize(null, null);
+        }
+
+
+        [TestMethod]
+        [ExpectedException(typeof(FilterTypeMismatchException))]
+        public void Initialize_LastFilterOutputTypeDoesNotMatchPipelineOutput_ExceptionThrown()
+        {
+            var pipeline = new FilterPipeline<A, B, object>
+            (
+                new NullFilter<A, B, object>(),
+                new NullFilter<B, A, object>()
+            );
+
+            pipeline.Initialize(null, null);
+        }
+
+
+        [TestMethod]
+        [ExpectedException(typeof(FilterTypeMismatchException))]
+        public void Initialize_InputOutputTypeDoesNotMatchBetweenFilters_ExceptionThrown()
+        {
+            var pipeline = new FilterPipeline<A, B, object>
+            (
+                new NullFilter<A, B, object>(),
+                new NullFilter<A, B, object>()
+            );
+
+            pipeline.Initialize(null, null);
+        }
+
+
+        [TestMethod]
+        public void Initialize_FilterInputIsSubclassOfOutputTypeOfPreviousFilter()
+        {
+            var pipeline = new FilterPipeline<A, B, object>
+            (
+                new NullFilter<A, SubB, object>(),
+                new NullFilter<B, B, object>()
+            );
+
+            pipeline.Initialize(null, null);
+        }
+    }
+}
